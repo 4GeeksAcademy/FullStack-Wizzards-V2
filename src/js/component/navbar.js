9a76a2e01dace70a22cc372fbd51ac530866549d
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2023-05-05 23:45:48 +0000

    :zap: Frirst Commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav className="navbar navbar-light bg-dark text-light mb-3">
+			<Link to="/" className="mx-2">
+				<span className="navbar-brand mb-0 h1 text-light">Router Practice 🧙</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary mx-2">Sign In</button>
 				</Link>
 			</div>
 		</nav>
